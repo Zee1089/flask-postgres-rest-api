5d9a761653e54f368b93fc1abd479eb151cba9f2
Zee <zeeboo696@gmail.com>
2024-08-24 13:00:42 -0400

    setting up jwts

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
+from flask_cors import CORS
 from dotenv import load_dotenv
 import os
 import psycopg2
 import psycopg2.extras
+import jwt
 
 
 load_dotenv()
 
 #  Initialize Flask, We'll use the pre-defined global '__name__' variable to tell Flask where it is.
 app = Flask(__name__)
+CORS(app)
 
 
 # app.py
@@ -21,6 +24,26 @@ def get_db_connection():
     )
     return connection
 
+# app.py
+@app.route('/sign-token')
+def sign_token():
+    user = {
+        "id": 1,
+        "username": "test",
+        "password": "test"
+    }
+    token = jwt.encode(user, os.getenv('JWT_SECRET'), algorithm="HS256")
+    # return token
+    return jsonify({"token": token})
+
+@app.route('/verify-token', methods=['POST'])
+def verify_token():
+    try:
+        token = request.headers.get('Authorization').split(' ')[1]
+        decoded_token = jwt.decode(token, os.getenv('JWT_SECRET'), algorithms=["HS256"])
+        return jsonify({"user": decoded_token})
+    except Exception as error:
+       return jsonify({"error": error.message})
 
 # Define our route, This syntax is using a Python decorator, which is essentially a succinct way to wrap a function in another function.
 @app.route('/')
